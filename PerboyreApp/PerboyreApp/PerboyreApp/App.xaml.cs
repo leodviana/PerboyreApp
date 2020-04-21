2685c47b32b7f4bb0f73c53d4e811b184c124b54
Leonardo viana <damasceno.leonardo@gmail.com>
2020-04-20 21:53:46 -0300

    Criacao do projeto

---
+++ PerboyreApp/PerboyreApp/PerboyreApp/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PerboyreApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+#DEBUG
+            HotReloader.Current.Run(this);
+#endif
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
