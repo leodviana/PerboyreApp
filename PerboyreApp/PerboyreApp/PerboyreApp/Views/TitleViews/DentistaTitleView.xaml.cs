f149353b32467ba5dc8f99928c5d5d6c3af1db85
Leonardo Viana <damasceno.leonardo@gmail.com>
2020-04-21 12:22:23 -0300

    Criacao das views e viewsmodels

---
+++ PerboyreApp/PerboyreApp/PerboyreApp/Views/TitleViews/DentistaTitleView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace PerboyreApp.Views.TitleViews
+{
+    public partial class DentistaTitleView : ContentView
+    {
+        public DentistaTitleView()
+        {
+            InitializeComponent();
+        }
+    }
+}
